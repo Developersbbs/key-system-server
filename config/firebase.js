e2c0723b253786cda2603fcfdc6f9a1b48055b6f
Sathish R <sathishravichandran9345@gmail.com>
2025-09-29 15:40:44 +0530

    firebase-sdk-to-dotenv

--- config/firebase.js
+++ config/firebase.js
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 
 // IMPORTANT: Download the service account key from Firebase console and place it in this directory
 // with the exact name: 'key-systems-client-98765-firebase-adminsdk-fbsvc-718de2ab59.json'
-const serviceAccount = require('./key-systems-client-98765-firebase-adminsdk-fbsvc-4418c96c99.json');
+const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
